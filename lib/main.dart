2e979ffd6b25b8e94764c99db40cdb4832e6aa3e
Muhammad Aiman <76811510+i900aiman@users.noreply.github.com>
2023-12-24 15:29:53 +0800

    awak tekan bnyak kali

--- lib/main.dart
+++ lib/main.dart
@@ -80,7 +80,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'awak tekan banyak kali:',
             ),
             Text(
               '$_counter',
